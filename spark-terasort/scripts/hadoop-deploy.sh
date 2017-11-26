933b0c13bc9c967decef413d26bdde681252e773
Chameleon <chameleon@magentafork>
2017-11-26 06:15:29 +0000

    still fixing the export bug

--- spark-terasort/scripts/hadoop-deploy.sh
+++ spark-terasort/scripts/hadoop-deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-source ~/.bashrc
+export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64
+export HADOOP_PREFIX=$(pwd)/download/hadoop-2.7.4
 
 HADOOP_CONF_DIR=$HADOOP_PREFIX/etc/hadoop
 
