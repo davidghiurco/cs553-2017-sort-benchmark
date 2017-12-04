34e3e2e1e44a2bb2e3e6f7cdbd3311ec113aaf3e
David Ghiurco <dm.ghiurco@gmail.com>
2017-12-04 03:28:24 -0600

    New Spark implementation

--- spark-terasort/src/main/java/STerasort.java
+++ spark-terasort/src/main/java/STerasort.java
@@ -1,7 +1,20 @@
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.Function;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.examples.terasort.TeraInputFormat;
+import org.apache.hadoop.examples.terasort.TeraOutputFormat;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class STerasort {
 
@@ -16,14 +29,33 @@ public class STerasort {
         String hdfs_URI = args[2]; // "localhost:9000"
         int numPartitions = Integer.parseInt(args[3]);
 
-        SparkConf conf = new SparkConf().setAppName("Spark TeraSort");
-        JavaSparkContext sc = new JavaSparkContext(conf);
+        // Hadoop setup
+        Configuration hadoop_conf = new Configuration();
+        Job job = Job.getInstance(hadoop_conf, "STerasort");
 
-        JavaRDD<String> textFile = sc.textFile("hdfs://" + hdfs_URI + inputFileName);
-        JavaRDD<String> sorted = textFile
-                .sortBy((Function<String, String>) value -> value, true, numPartitions);
+        // Input
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        job.setInputFormatClass(TeraInputFormat.class);
 
-        sorted.saveAsTextFile("hdfs://" + hdfs_URI + outputDir);
+        // Output
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        job.setOutputFormatClass(TeraOutputFormat.class);
+
+
+        SparkConf spark_conf = new SparkConf().setAppName("Spark TeraSort");
+        JavaSparkContext sc = new JavaSparkContext(spark_conf);
+
+
+
+//        JavaRDD<String> textFile = sc.textFile("hdfs://" + hdfs_URI + inputFileName);
+//        JavaRDD<String> sorted = textFile
+//                .sortBy((Function<String, String>) value -> value, true, numPartitions);
+//        sorted.saveAsTextFile("hdfs://" + hdfs_URI + outputDir);
+
+        JavaPairRDD<Text, Text> rdd = sc.newAPIHadoopRDD(hadoop_conf, TeraInputFormat.class, Text.class, Text.class);
+        JavaPairRDD<Text, Text> sort = rdd.sortByKey();
+        sort.saveAsNewAPIHadoopDataset(hadoop_conf);
+
     }
 
     private static void usage() {
