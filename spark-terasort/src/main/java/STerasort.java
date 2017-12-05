756961987eec8576c0cfb732b922468aa1784194
David Ghiurco <dm.ghiurco@gmail.com>
2017-12-05 03:45:43 -0600

    Adding timing info

--- spark-terasort/src/main/java/STerasort.java
+++ spark-terasort/src/main/java/STerasort.java
@@ -3,6 +3,8 @@ import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.Function;
 
+import java.io.PrintWriter;
+
 public class STerasort {
 
     public static void main(String[] args) throws Exception {
@@ -13,10 +15,19 @@ public class STerasort {
         SparkConf spark_conf = new SparkConf().setAppName("Spark TeraSort");
         JavaSparkContext sparkContext = new JavaSparkContext(spark_conf);
 
+
+        long startTime = System.nanoTime();
         JavaRDD<String> textFile = sparkContext.textFile("/input");
         JavaRDD<String> sorted = textFile
                 .sortBy((Function<String, String>) value -> value, true, Integer.parseInt(args[0]));
         sorted.saveAsTextFile("/output");
+        long endTime = System.nanoTime();
+
+        double duration = (double) (endTime - startTime) / 1000000000.0;
+
+        PrintWriter writer = new PrintWriter("stera_duration.txt", "UTF-8");
+        writer.println(duration);
+        writer.close();
 
         sparkContext.stop();
 
