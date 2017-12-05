b660e48d9edd43d0a5ab9a84551726ed67c34d8f
David Ghiurco <dm.ghiurco@gmail.com>
2017-12-05 06:05:32 -0600

    Reverting to old implementation

--- spark-terasort/src/main/java/STerasort.java
+++ spark-terasort/src/main/java/STerasort.java
@@ -1,12 +1,6 @@
 import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.api.java.function.Function;
-import org.apache.spark.input.PortableDataStream;
-import org.codehaus.janino.Java;
-
-import java.io.*;
 
 public class STerasort {
 
@@ -20,25 +14,10 @@ public class STerasort {
 
 
         long startTime = System.nanoTime();
-//        JavaRDD<String> textFile = sparkContext.textFile("/input", Integer.parseInt(args[0]));
-//        JavaRDD<String> sorted = textFile
-//                .sortBy(value -> value, true, Integer.parseInt(args[0]));
-//        sorted.saveAsTextFile("/output");
-
-        JavaRDD<byte[]> rdd = sparkContext.binaryRecords("/input", 100);
-        JavaRDD<byte[]> sorted = rdd
-                .sortBy(new Function<byte[],String>() {
-                    @Override
-                    public String call(byte[] v1) throws Exception {
-                        return new String(v1);
-                    }
-                }, true, Integer.parseInt(args[0]));
+        JavaRDD<String> textFile = sparkContext.textFile("/input", Integer.parseInt(args[0]));
+        JavaRDD<String> sorted = textFile
+                .sortBy(value -> value, true, Integer.parseInt(args[0]));
         sorted.saveAsTextFile("/output");
-
-
-//        JavaPairRDD<String, PortableDataStream> binaryFile = sparkContext.binaryFiles("/input");
-//        JavaPairRDD<String, PortableDataStream> sorted = binaryFile.sortByKey();
-//        sorted.saveAsObjectFile("/output");
 
 
         long endTime = System.nanoTime();
@@ -46,14 +25,6 @@ public class STerasort {
 
         System.out.println("***************DURATION: " + duration + "s");
 
-//        PrintWriter writer = new PrintWriter("stera_duration.txt", "UTF-8");
-//        writer.println(duration);
-//        writer.close();
-//
-//        try (Writer w = new BufferedWriter(new OutputStreamWriter(
-//                new FileOutputStream("filename.txt"), "utf-8"))) {
-//            w.write("something");
-//        }
 
     }
 
