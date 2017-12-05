531dd9305ea810e9686975e8661d45c7c5e95ec1
David Ghiurco <dm.ghiurco@gmail.com>
2017-12-05 05:25:35 -0600

    Trying binary output

--- spark-terasort/src/main/java/STerasort.java
+++ spark-terasort/src/main/java/STerasort.java
@@ -1,8 +1,10 @@
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.input.PortableDataStream;
 
-import java.io.PrintWriter;
+import java.io.*;
 
 public class STerasort {
 
@@ -16,19 +18,30 @@ public class STerasort {
 
 
         long startTime = System.nanoTime();
-        JavaRDD<String> textFile = sparkContext.textFile("/input", Integer.parseInt(args[0]));
-        JavaRDD<String> sorted = textFile
-                .sortBy(value -> value, true, Integer.parseInt(args[0]));
-        sorted.saveAsTextFile("/output");
-        long endTime = System.nanoTime();
+//        JavaRDD<String> textFile = sparkContext.textFile("/input", Integer.parseInt(args[0]));
+//        JavaRDD<String> sorted = textFile
+//                .sortBy(value -> value, true, Integer.parseInt(args[0]));
+//        sorted.saveAsTextFile("/output");
 
+
+        JavaPairRDD<String, PortableDataStream> binaryFile = sparkContext.binaryFiles("/input");
+        JavaPairRDD<String, PortableDataStream> sorted = binaryFile.sortByKey();
+        sorted.saveAsObjectFile("/output");
+
+
+        long endTime = System.nanoTime();
         double duration = (double) (endTime - startTime) / 1000000000.0;
 
-        PrintWriter writer = new PrintWriter("stera_duration.txt", "UTF-8");
-        writer.println(duration);
-        writer.close();
+        System.out.println("***************DURATION: " + duration + "s");
 
-        sparkContext.stop();
+//        PrintWriter writer = new PrintWriter("stera_duration.txt", "UTF-8");
+//        writer.println(duration);
+//        writer.close();
+//
+//        try (Writer w = new BufferedWriter(new OutputStreamWriter(
+//                new FileOutputStream("filename.txt"), "utf-8"))) {
+//            w.write("something");
+//        }
 
     }
 
